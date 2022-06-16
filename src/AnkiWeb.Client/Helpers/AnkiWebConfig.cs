daa1a54e564bd85fbe651fb453a7f004c54d0cb6
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2022-06-16 15:36:11 +0200

    Create unit test for AnkiWebConfig and fix bug with IsConfigured property

--- src/AnkiWeb.Client/Helpers/AnkiWebConfig.cs
+++ src/AnkiWeb.Client/Helpers/AnkiWebConfig.cs
@@ -5,7 +5,7 @@ internal class AnkiWebConfig
     public string AnkiUserCsrfToken { get; set; } = string.Empty;
     public bool AnkiCookieExists { get; set; } = false;
 
-    public bool IsConfigured { get => (string.IsNullOrEmpty(AnkiWebCsrfToken) & string.IsNullOrEmpty(AnkiUserCsrfToken) & AnkiCookieExists); }
+    public bool IsConfigured { get => !string.IsNullOrEmpty(AnkiWebCsrfToken) && !string.IsNullOrEmpty(AnkiUserCsrfToken) && AnkiCookieExists; }
 
     public AnkiWebConfig()
     {
---
+++ tests/AnkiWeb.Client.Tests/Helpers/AnkiWebConfig_Tests.cs
@@ -0,0 +1,18 @@
+﻿using AnkiWeb.Client.Helpers;
+
+namespace AnkiWeb.Client.Tests.Helpers;
+
+public class AnkiWebConfig_Tests
+{
+    [Fact]
+    public void IsConfigured_Returns_Correct_Value()
+    {
+        AnkiWebConfig config = new();
+
+        config.AnkiCookieExists = true;
+        config.AnkiWebCsrfToken = "12345";
+        config.AnkiUserCsrfToken = "12345";
+
+        Assert.True(config.IsConfigured);
+    }
+}
